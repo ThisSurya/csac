44167cba41a74ad4fd72b7caced684097eb3a4e4
thisSurya <magang4@psi.dinus.ac.id>
2024-08-12 15:00:32 +0700

    FIne

--- resources/js/Pages/Admin/LandingText/Index.jsx
+++ resources/js/Pages/Admin/LandingText/Index.jsx
@@ -5,8 +5,7 @@ import { useEffect, useRef, useState } from "react";
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Button } from 'primereact/button';
-import Modal from "@/Components/Modal";
-import SecondaryButton from "@/Components/SecondaryButton";
+import HelperCard from '@/Components/HelperCard';
 
 const renderDisplay = () => {
     const [results, setResult] = useState([]);
@@ -32,8 +31,12 @@ const renderDisplay = () => {
         <div className="flex">
             <AuthenticatedLayout />
             <div className="flex flex-col w-full">
-                <div className="h-16 my-4 ml-4">
-                    <h1 className="text-gray-500 text-xl font-bold">Current Page: Dashboard Content editore</h1>
+                <div className="h-16 my-4 ml-4 flex">
+                    <h1 className="text-gray-500 text-xl font-bold mr-2">Current Page: Dashboard Content editor</h1>
+                    <HelperCard
+                        header={<h1>Dashboard Content Editor</h1>}
+                        content={<p>Dashboard content editor adalah bagian yg mengotrol teks utama (tepat dibawah judul persis) kamu bisa mendeksripsikan dari setiap page pada page ini. Terdiri 7 bagian page</p>}
+                    />
                 </div>
 
                 <div className="bg-white shadow-lg rounded-lg border-gray-300 border-2  mx-2 mb-2">
